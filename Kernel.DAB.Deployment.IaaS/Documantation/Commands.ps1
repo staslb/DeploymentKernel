20bf382aaf3cf9037a601f78f00d169e1f8efc47
Ostap <aresall1990@gmail.com>
2017-11-22 22:08:43 +0200

    rewrite DNS configuration

--- Kernel.DAB.Deployment.IaaS/Documantation/Commands.ps1
+++ Kernel.DAB.Deployment.IaaS/Documantation/Commands.ps1
@@ -2,3 +2,7 @@
 # Commands.ps1
 #
 Test-NetConnection -ComputerName automationkerneliaas.file.core.windows.net -Port 445
+
+PS C:\Users\ostapkravchuk> Compress-Archive -U -Path C:\Users\ostapkravchuk\source\repos\Kernel.DAB.Deployment.IaaS\Kern
+el.DAB.Deployment.IaaS\dsc\configureADDNS.ps1\* -DestinationPath C:\Users\ostapkravchuk\source\repos\Kernel.DAB.Deployme
+nt.IaaS\Kernel.DAB.Deployment.IaaS\dsc\configureADDNS.ps1.zip
---
+++ Kernel.DAB.Deployment.IaaS/dsc/configureADDNS.ps1/configureADDNS.ps1
@@ -0,0 +1,93 @@
+﻿Configuration configureADDNS {
+param
+    #v1.4
+   ( 
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$AdminCreds,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$myFirstUserCreds,
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    )
+
+Import-DscResource -ModuleName xActiveDirectory, xNetworking, xPendingReboot
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+
+    Node localhost
+    {
+        LocalConfigurationManager            
+        {            
+            ActionAfterReboot = 'ContinueConfiguration'            
+            ConfigurationMode = 'ApplyOnly'            
+            RebootNodeIfNeeded = $true            
+        } 
+
+        WindowsFeature DNS 
+        { 
+            Ensure = "Present" 
+            Name = "DNS"
+        }
+
+        xDnsServerAddress DnsServerAddress 
+        { 
+            Address        = '127.0.0.1' 
+            InterfaceAlias = 'Ethernet'
+            AddressFamily  = 'IPv4'
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        WindowsFeature RSAT
+        {
+            Ensure = "Present"
+            Name = "RSAT"
+        }
+
+        WindowsFeature ADDSInstall 
+        { 
+            Ensure = "Present" 
+            Name = "AD-Domain-Services"
+        }  
+
+        xADDomain FirstDS 
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DatabasePath = "C:\NTDS"
+            LogPath = "C:\NTDS"
+            SysvolPath = "C:\SYSVOL"
+            DependsOn = "[WindowsFeature]ADDSInstall","[xDnsServerAddress]DnsServerAddress"
+        }
+
+        xWaitForADDomain DscForestWait
+        {
+            DomainName = $DomainName
+            DomainUserCredential = $DomainCreds
+            RetryCount = $RetryCount
+            RetryIntervalSec = $RetryIntervalSec
+            DependsOn = "[xADDomain]FirstDS"
+        } 
+
+        xADUser FirstUser 
+        { 
+            DomainName = $DomainName 
+            DomainAdministratorCredential = $DomainCreds 
+            UserName = $myFirstUserCreds.Username 
+            Password = $myFirstUserCreds
+            Ensure = "Present" 
+            DependsOn = "[xWaitForADDomain]DscForestWait" 
+        } 
+
+        xPendingReboot Reboot1
+        { 
+            Name = "RebootServer"
+            DependsOn = "[xWaitForADDomain]DscForestWait"
+        }
+
+   }
+}
